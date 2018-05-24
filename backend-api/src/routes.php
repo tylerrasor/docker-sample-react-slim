27b1e77dac07e8de20b41ddbcc7a402fe0967996
rasor <rasor.tyler@gmail.com>
2018-05-23 21:55:28 -0400

    basic api

--- backend-api/src/routes.php
+++ backend-api/src/routes.php
@@ -5,10 +5,54 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+$app->get('/basicTitle[/{offset}]', function ($request, $response, $args) {
+    $sth = $this->db->prepare("
+        SELECT id, primaryTitle 
+        FROM titles
+        ORDER BY primaryTitle
+        LIMIT 1
+        OFFSET 1 * :offset;");
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    $offset = $args['offset'] ? $args['offset'] : '0';
+    $sth->bindParam('offset', $offset);
+
+    $sth->execute();
+    return $this->response->withJson($sth->fetchAll());
+});
+
+$app->get('/fullTitle/{id}', function($request, $response, $args) {
+    $sth = $this->db->prepare("
+        SELECT *
+        FROM titles
+        WHERE id = :id;");
+
+    $sth->bindParam('id', $args['id']);
+
+    $sth->execute();
+    return $this->response->withJson($sth->fetchAll());
+});
+
+$app->get('/search/{searchString}[/{pageNum}]', function($request, $response, $args) {
+    $sth = $this->db->prepare("
+        SELECT id, primaryTitle
+        FROM titles
+        WHERE primaryTitle ILIKE :searchString
+        LIMIT 10
+        OFFSET 10 * :pageNum;");
+
+    $searchString = $args['searchString'] . '%';
+    $sth->bindParam('searchString', $searchString);
+
+    $pageNum = $args['pageNum'] ? $args['pageNum'] : '0';
+    $sth->bindParam('pageNum', $pageNum);
+
+    $sth->execute();
+    return $this->response->withJson($sth->fetchAll());
+});
+
+// Catch-all route to serve a 404 Not Found page if none of the routes match
+// NOTE: make sure this route is defined last
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+    $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+    return $handler($req, $res);
 });
